26b071d7e84bfab2e011a58be42a1183995ba720
IvanCacha <ivan.cachafeiro@gmail.com>
2019-11-30 19:06:27 +0100

    se añade el tamaño del fichero en forma de dictionario y se ordena de forma descendente

--- List_Order_File_Directory.py
+++ List_Order_File_Directory.py
@@ -7,17 +7,26 @@ if len(sys.argv)>=2:
     dir_path=sys.argv[1]
 else:
     dir_path=os.path.dirname(os.path.realpath(__file__))
-    
+
+if len(sys.argv)>=3:
+    flag_subdirectory=sys.argv[2]
+else:
+    flag_subdirectory='Y'
+
 print('El directorio establecido es: ' + dir_path)
 
 files = []
+
 for r, d, f in os.walk(dir_path):
     for file in f:
         files.append(os.path.join(r, file))
 
-for f in files:
-    print(f)
+for i in range(len(files)):
+        files[i] = (files[i], os.path.getsize(files[i]))
 
+files.sort(key=lambda filename: filename[1], reverse=True)
+
+print(files)
 
 
 # %%
