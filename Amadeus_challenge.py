1af64ab0da7559825f398e68abbfd992ef4666b1
IvanCacha <ivan.cachafeiro@gmail.com>
2020-01-25 09:57:59 +0100

    Sample file creation

--- Amadeus_challenge.py
+++ Amadeus_challenge.py
@@ -9,14 +9,21 @@ bookings_file = directory + 'bookings.csv.bz2'
 # %%
 df_booking=pd.read_csv(bookings_file, 
                        compression='bz2',
-                       header=0,
-                       chunksize=100,
-                       sep='^')
+                       header=None,
+                       chunksize=500000)
 df_search = pd.read_csv(searches_file,
                        compression='bz2',
-                       header=0,
-                       chunksize=100,
-                       sep='^')
+                       header=None,
+                       chunksize=500000)
 
+
+# %%
+sample_bookings=df_booking.get_chunk(10000)
+sample_bookings.to_csv(directory+ 'Sample_Bookings.csv.bz2',compression='bz2')
+sample_searches=df_search.get_chunk(10000)
+sample_searches.to_csv(directory+ 'Sample_Searches.csv.bz2',compression='bz2')
+
+# %%
+sample_bookings
 
 # %%
