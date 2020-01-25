db1ef53c8c6c9280d0c8b19a13b32313e0b98133
IvanCacha <ivan.cachafeiro@gmail.com>
2020-01-25 13:14:36 +0100

    porgressing exercice 2

--- Amadeus_challenge_2.py
+++ Amadeus_challenge_2.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 
 directory = "C:\\Users\\ivanc\\OneDrive\\Documents\\Master Data Science\\Amadeus Challenge\\"
-bookings_file = directory + 'Sample_Bookings.csv.bz2'
+bookings_sample = directory + 'Sample_Bookings.csv.bz2'
 
 # %%
-df_booking = pd.read_csv(bookings_file, sep='^', header=0,
+df_booking = pd.read_csv(bookings_sample, sep='^', header=0,
                        compression='bz2')
 
 
@@ -46,3 +46,26 @@ result
 top10 = result.iloc[:10]
 top10
 # %%
+bookings_file = directory + 'bookings.csv.bz2'
+df_booking = pd.read_csv(bookings_file,
+                         sep='^',
+                         usecols=['arr_port','pax', 'year'],
+                         chunksize=200000
+                         )
+# %%
+all_bookings=pd.DataFrame()
+for i, chunks in enumerate(df_booking):
+    print(i)
+    year_filter = chunks['year'] == 2013
+    result_chunks = chunks[year_filter][['arr_port','pax']].groupby('arr_port').sum()
+    result_chunks.reset_index(inplace=True)
+    all_bookings = all_bookings.append(result_chunks)
+all_bookings.shape      
+
+# %%
+result= all_bookings.groupby('arr_port').sum().sort_values('pax',ascending=False)
+top10 = result.iloc[:10]
+top10
+# %%
+
+
